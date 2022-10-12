91dcf7300c3073fcd236cd48518e8e1ba869a1b9
Yurii Dvornichenko <raen98@live.ru>
2022-10-12 14:40:31 +0300

    finish 5 homework

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,11 @@ const e = require('express');
 const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcryptjs');
+const path = require('path');
+const fs = require('fs').promises;
+const Jimp = require('jimp');
+
+const avatarsDir = path.join(__dirname, '../../', 'public', 'avatars');
 
 const { Users } = require('../../db/usersModel');
 
@@ -11,9 +16,10 @@ const {
   getCurrent,
   logout,
   updateStatusUser,
+  updateAvatar,
 } = require('../../models/users');
 
-const { MiddlewaresUsers, auth } = require('../../middlewares');
+const { MiddlewaresUsers, auth, upload } = require('../../middlewares');
 
 router.post(
   '/signup',
@@ -29,11 +35,13 @@ router.post(
       }
 
       const newUser = await register(req.body);
+      console.log(newUser.avatarURL);
 
       res.status(201).json({
         user: {
           email: newUser.email,
           subscription: newUser.subscription,
+          avatarURL: newUser.avatarURL,
         },
       });
     } catch (error) {
@@ -70,7 +78,7 @@ router.post(
   }
 );
 
-router.get('/current', auth.auth, async (req, res, next) => {
+router.get('/current', auth, async (req, res, next) => {
   const { email, subscription } = await getCurrent(req.user);
 
   res.status(200).json({
@@ -81,7 +89,7 @@ router.get('/current', auth.auth, async (req, res, next) => {
   });
 });
 
-router.get('/logout', auth.auth, async (req, res, next) => {
+router.get('/logout', auth, async (req, res, next) => {
   const user = await logout(req.user);
 
   if (!user) {
@@ -93,7 +101,7 @@ router.get('/logout', auth.auth, async (req, res, next) => {
 
 router.patch(
   '/subscription',
-  auth.auth,
+  auth,
   MiddlewaresUsers.putchValidationSubscription,
   async (req, res, next) => {
     try {
@@ -113,6 +121,43 @@ router.patch(
         },
       });
     } catch (error) {
+      res.status(500).json({ error: e.message });
+    }
+  }
+);
+
+router.patch(
+  '/avatars',
+  auth,
+  upload.single('avatar'),
+  async (req, res, next) => {
+    const { path: tempUpload, originalname } = req.file;
+    const { _id: id } = req.user;
+    const imageName = `${id}_${originalname}`;
+
+    try {
+      const img = await Jimp.read(tempUpload);
+      await img
+        .autocrop()
+        .cover(
+          250,
+          250,
+          Jimp.HORIZONTAL_ALIGN_CENTER || Jimp.VERTICAL_ALIGN_MIDDLE
+        )
+        .writeAsync(tempUpload);
+
+      const resuldUpload = path.join(avatarsDir, imageName);
+      await fs.rename(tempUpload, resuldUpload);
+      const avatarURL = path.join('public', 'avatars', imageName);
+
+      const newAvatar = await updateAvatar(id, avatarURL);
+      res.status(200).json({
+        user: {
+          avatarURL: newAvatar.avatarURL,
+        },
+      });
+    } catch (error) {
+      await fs.unlink(tempUpload);
       res.status(500).json({ error: e.message });
     }
   }
